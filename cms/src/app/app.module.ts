3f91d1e6488d3f13fbb0204621155f22835f7a8c
Tauriel1202 <lothlorienexplorer20@gmail.com>
2023-01-13 21:10:18 -0500

    cms lsn 2 html and ts done; still working on css

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
     ContactListComponent,
-    ContactDetailComponent
+    ContactDetailComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
 })
 export class ContactListComponent {
-
+  contacts: Contact[] = [
+    new Contact(
+      1,
+      'Galadriel Felagund',
+      'supercommander@lindonlorien.elf',
+      '123-456-7893',
+      'https://www.looper.com/img/gallery/galadriels-family-on-the-rings-of-power-explained/intro-1665155261.jpg',
+      'null'
+    ),
+    new Contact(
+      2,
+      'Tauriel Greenleaf',
+      'elfcaptain@mirkwood.elf',
+      '456-789-1239',
+      'https://upload.wikimedia.org/wikipedia/en/thumb/4/49/EvangelineLillyAsTauriel.jpg/220px-EvangelineLillyAsTauriel.jpg',
+      'null'
+    ),
+  ];
 }
