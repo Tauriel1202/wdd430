151c30b0d8114a9a73a1f4210859dd13c83f8c20
Tauriel1202 <lothlorienexplorer20@gmail.com>
2023-01-14 17:33:37 -0500

    css and imgs up and running

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -10,19 +10,20 @@ export class ContactListComponent {
   contacts: Contact[] = [
     new Contact(
       1,
-      'Galadriel Felagund',
-      'supercommander@lindonlorien.elf',
-      '123-456-7893',
-      'https://www.looper.com/img/gallery/galadriels-family-on-the-rings-of-power-explained/intro-1665155261.jpg',
-      'null'
+      "R. Kent Jackson",
+      "jacksonk@byui.edu",
+      "208-496-3771",
+      "../../assets/imgs/jacksonk.jpg",
+      null
     ),
-    new Contact(
+    new Contact (
       2,
-      'Tauriel Greenleaf',
-      'elfcaptain@mirkwood.elf',
-      '456-789-1239',
-      'https://upload.wikimedia.org/wikipedia/en/thumb/4/49/EvangelineLillyAsTauriel.jpg/220px-EvangelineLillyAsTauriel.jpg',
-      'null'
+      "Rex Barzee",
+      "barzeer@byui.edu",
+      "208-496-3768",
+      "../../assets/imgs/barzeer.jpg",
+      null
     ),
+    
   ];
 }
