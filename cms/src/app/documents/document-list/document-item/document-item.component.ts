e525894bfbd1956a0e464bd70e26b6281fb2ac1d
Tauriel1202 <lothlorienexplorer20@gmail.com>
2023-01-24 20:23:13 -0500

    documents working

--- cms/src/app/documents/document-list/document-item/document-item.component.ts
+++ cms/src/app/documents/document-list/document-item/document-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Document } from '../../documents.model';
 
 @Component({
   selector: 'cms-document-item',
   templateUrl: './document-item.component.html',
-  styleUrls: ['./document-item.component.css']
+  styleUrls: ['./document-item.component.css'],
 })
 export class DocumentItemComponent {
+  @Input() document!: Document;
+  @Output() documentSelected = new EventEmitter<void>();
 
+  onSelected() {
+    this.documentSelected.emit();
+  }
 }
--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../documents.model';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  styleUrls: ['./document-list.component.css'],
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document(
+      1,
+      'Recipe Book',
+      'an example project with recipes',
+      '../../RecipeBook',
+      []
+    ),
+    new Document(
+      2,
+      'Alvena',
+      'a personal writing project',
+      'example/url.docx',
+      []
+    ),
+    new Document(
+      3,
+      'Img testing space',
+      'a free space for messing around with imgs and building posters',
+      'example/url.pptx',
+      []
+    ),
+    new Document(
+      4,
+      'Characters',
+      'a chart for keeping track of characters',
+      'example/url.exe',
+      []
+    ),
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
