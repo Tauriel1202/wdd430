a84fb9293d68b61c5f68cd27ddcd0cffe4ab947b
Tauriel1202 <lothlorienexplorer20@gmail.com>
2023-03-21 17:56:01 -0400

    header working, chars started

--- MiddleEarth/src/app/chars/chars.component.ts
+++ MiddleEarth/src/app/chars/chars.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chars',
   templateUrl: './chars.component.html',
-  styleUrls: ['./chars.component.css']
+  styleUrls: ['./chars.component.css'],
 })
-export class CharsComponent {
+export class CharsComponent implements OnInit {
+  // selectedChar: CharsComponent;
 
+  // constructor(private charService: CharService) {}
+
+  ngOnInit() {
+    // this.charService.
+    console.log(
+      'The Elves found the Char Component and are partying On-In-It!'
+    );
+  }
 }
